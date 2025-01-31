3bf96da5b5e7c4e407479acff69682957eb54e3d
Octavian Torcea <ot2437@tobii.com>
2025-01-31 06:44:50 +0200

    refactor reservation method to use a queue

--- backend/reservation/reservation.py
+++ backend/reservation/reservation.py
@@ -1,6 +1,8 @@
 import os
-import time
+from concurrent.futures import Future
 from datetime import date, timedelta, datetime
+from queue import Queue
+from threading import Thread
 
 import uvicorn
 from dotenv import load_dotenv
@@ -19,59 +21,74 @@ app = FastAPI()
 
 # TODO: add token verification, reserve room only for teachers
 
+reservation_queue = Queue()
+
+
+def _process_reservations():
+    while True:
+        res_future, reservation = reservation_queue.get(block=True)
+
+        room_name = reservation.room_name
+        start_date = reservation.start_date
+        end_date = reservation.end_date
+        reserved_by = reservation.name
+
+        day_of_reservation = start_date.date()
+        start_time = start_date.time()
+        end_time = end_date.time()
+
+        if start_time >= end_time:
+            res_future.set_exception(HTTPException(status_code=400, detail="Start time must be before end time."))
+            continue
+
+        RoomReservation = create_room_table(room_name)
+
+        db = SessionLocal()
+
+        try:
+            inspector = inspect(engine)
+            if not inspector.has_table(room_name):
+                Base.metadata.create_all(bind=engine, tables=[RoomReservation.__table__])
+
+            overlapping_reservations = db.query(RoomReservation).filter(
+                RoomReservation.day_of_reservation == day_of_reservation,
+                RoomReservation.start_time < end_date,
+                RoomReservation.end_time > start_time
+            ).all()
+
+            if overlapping_reservations:
+                res_future.set_exception(HTTPException(status_code=400, detail="Time slot is already booked."))
+                continue
+
+            reservation = RoomReservation(
+                day_of_reservation=day_of_reservation,
+                start_time=start_time,
+                end_time=end_time,
+                reserved_by=reserved_by
+            )
+            db.add(reservation)
+
+            db.commit()
+
+            res_future.set_result({"message": "Reservation successfully!", "reservation_id": reservation.id})
+
+        except Exception as e:
+            db.rollback()
+            res_future.set_exception(HTTPException(status_code=500, detail=str(e)))
+
+        finally:
+            db.close()
+            reservation_queue.task_done()
+
 
 @app.post("/reserve")
 def reserve_room(
     reservation: ReservationRequest
 ):
-    room_name = reservation.room_name
-    start_date = reservation.start_date
-    end_date = reservation.end_date
-    reserved_by = reservation.name
+    res_future = Future()
+    reservation_queue.put((res_future, reservation))
 
-    day_of_reservation = start_date.date()
-    start_time = start_date.time()
-    end_time = end_date.time()
-
-    if start_time >= end_time:
-        raise HTTPException(status_code=400, detail="Start time must be before end time.")
-
-    RoomReservation = create_room_table(room_name)
-
-    db = SessionLocal()
-
-    try:
-        inspector = inspect(engine)
-        if not inspector.has_table(room_name):
-            Base.metadata.create_all(bind=engine, tables=[RoomReservation.__table__])
-
-        overlapping_reservations = db.query(RoomReservation).filter(
-            RoomReservation.day_of_reservation == day_of_reservation,
-            RoomReservation.start_time < end_date,
-            RoomReservation.end_time > start_time
-        ).all()
-
-        if overlapping_reservations:
-            raise HTTPException(status_code=400, detail="Time slot is already booked.")
-
-        reservation = RoomReservation(
-            day_of_reservation=day_of_reservation,
-            start_time=start_time,
-            end_time=end_time,
-            reserved_by=reserved_by
-        )
-        db.add(reservation)
-
-        db.commit()
-
-        return {"message": "Reservation successfully!", "reservation_id": reservation.id}
-
-    except Exception as e:
-        db.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
-
-    finally:
-        db.close()
+    return res_future.result()
 
 
 def _get_reservations(db, RoomReservation, start_date, end_date) -> dict[date, list[ReservationResponse]]:
@@ -187,4 +204,5 @@ def show_reservations(
 
 
 if __name__ == '__main__':
+    Thread(target=_process_reservations, daemon=True).start()
     uvicorn.run(app, host=os.getenv("HOST"), port=int(os.getenv("PORT")))
